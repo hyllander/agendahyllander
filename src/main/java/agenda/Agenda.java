c08adcd4fb1fefa5135ca3146971973b74fe7efe
Hyllander-Gomes <126484132+hyllander@users.noreply.github.com>
2026-03-24 08:42:49 -0300

    Update Agenda.java

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -1,4 +1,10 @@
-package agenda;
+import java.util.Collection;
+import java.io.IOException;
 
 public interface Agenda {
+    public boolean cadastraContato(String nome, int dia, int mes);
+    public Collection<Contato> pesquisaAniversariantes(int dia, int mes);
+    public boolean removeContato(String nome) throws ContatoInexistenteException;
+    public void salvarDados() throws IOException;
+    public void recuperarDados() throws IOException;
 }
