34a9f85a9398e1996d698ba38ccee9612c656673
Hyllander-Gomes <126484132+hyllander@users.noreply.github.com>
2026-03-24 08:44:10 -0300

    Update Contato.java

--- src/main/java/agenda/Contato.java
+++ src/main/java/agenda/Contato.java
@@ -1,4 +1,29 @@
-package agenda;
+import java.io.Serializable;
 
-public class Contato {
+public class Contato implements Serializable {
+    private String nome;
+    private int dia;
+    private int mes;
+
+    public Contato(String nome, int dia, int mes) {
+        this.nome = nome;
+        this.dia = dia;
+        this.mes = mes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public String toString() {
+        return nome + " - " + dia + "/" + mes;
+    }
 }
