0bb37db9ac3899932e21e58ff0224e8364dd0f2a
Hyllander-Gomes <126484132+hyllander@users.noreply.github.com>
2026-03-24 08:45:54 -0300

    Update GravadorDeDados.java

--- src/main/java/agenda/GravadorDeDados.java
+++ src/main/java/agenda/GravadorDeDados.java
@@ -1,4 +1,24 @@
-package agenda;
+import java.io.*;
+import java.util.HashMap;
 
 public class GravadorDeDados {
+
+    private String arquivo = "agendahyllander.dat";
+
+    public void salvarDados(HashMap<String, Contato> contatos) throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(
+                new FileOutputStream(arquivo));
+        out.writeObject(contatos);
+        out.close();
+    }
+
+    public HashMap<String, Contato> recuperarDados() throws IOException {
+        try {
+            ObjectInputStream in = new ObjectInputStream(
+                    new FileInputStream(arquivo));
+            return (HashMap<String, Contato>) in.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new IOException(e);
+        }
+    }
 }
